d384bdfe72f127a6efeada12aa5f52dcf79a3855
Ânderson Matheus Flores De Oliveira <anderson.oliveira.ao844@gmail.com>
2025-07-11 08:20:03 -0300

    autor

--- app_simulador_caixas.py
+++ app_simulador_caixas.py
@@ -291,6 +291,7 @@ st.markdown("""
             <img src="https://raw.githubusercontent.com/MySpaceCrazy/Simulador_caixas_2D/refs/heads/main/linkedin.ico" alt="LinkedIn" style="width: 32px; height: 32px;">
         </a>
     </div>
+    <p class="footer-text">© 2025 Ânderson Oliveira. Todos os direitos reservados.</p>
 </div>
 """, unsafe_allow_html=True) 
 
